c9efd57952b38bb2d0cf2823f9b17708d0b52e1e
Tyler Terry <tvp7098@wmich.edu>
2023-07-12 20:36:01 -0400

    frontend stable beginning backend

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import { Express } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express()
+app.use(cors());
+app.use(express.json({ limig: '50mb'}));
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello from DALL.E' })
+});
