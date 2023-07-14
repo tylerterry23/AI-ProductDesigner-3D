7c331f7561d3607ef54d195bfc669fada2d88d3d
Tyler Terry <tvp7098@wmich.edu>
2023-07-13 22:25:04 -0400

    frontend deployment test

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -53,7 +53,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true);
 
-      const response = await fetch('https://ai-productdesigner-3d.onrender.com', {
+      const response = await fetch('https://ai-productdesigner-3d.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
